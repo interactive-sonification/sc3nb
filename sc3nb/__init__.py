bfa384b3cad599b4d60a660b11c353ad1fd4c47d
Thomas Hermann <thermann@techfak.uni-bielefeld.de>
2019-02-18 12:47:08 +0100

    remove play&record (native audio functions)

--- sc3nb/__init__.py
+++ sc3nb/__init__.py
@@ -4,5 +4,5 @@ as well as playing recording and visualizing audio
 """
 
 from .sc import startup, SC
-from .helpers import ampdb, clip, cpsmidi, dbamp, linlin, midicps, play, record
+from .helpers import ampdb, clip, cpsmidi, dbamp, linlin, midicps
 from .timed_queue import TimedQueue, TimedQueueSC
