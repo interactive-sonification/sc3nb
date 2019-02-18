486713585030d88073f6996c9e6bf4962cd73f8c
Ferdinand Schlatt <fschlatt@gmail.com>
2019-02-18 17:59:00 +0100

    add requirements

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
+with open('requirements.txt') as read_file:
+    REQUIRED = read_file.read().splitlines()
+
 setup(
     name='sc3nb',
     version='0.0.1',
     description='SuperCollider3 (sc3) jupyter notebook class and magics',
     license='MIT',
     packages=['sc3nb'],
+    install_requires=REQUIRED,
     author='Thomas Hermann',
     author_email='thermann@techfak.uni-bielefeld.de',
     keywords=['sonification, sound synthesis'],
