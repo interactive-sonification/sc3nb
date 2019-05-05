20bd27d280bd2c2ea9d3aab272479ffbcc4294fa
Steffen Micha Voße <github@gympap.de>
2019-05-05 14:18:29 +0200

    Buffer: more methods added

--- sc3nb/buffer.py
+++ sc3nb/buffer.py
@@ -6,22 +6,34 @@ from random import randint
 
 
 class Buffer:
-    def __init__(self, sc, data):
-        self.bufferId = randint(0,100000)
+    def __init__(self, sc, data, sr=44100):
+        self.bufnum = randint(0, 100000) # ToDo: Better num handling
         self.sc = sc
-        sp.io.wavfile.write('./temp_' + str(self.bufferId), 500, data)
-        self.sc.msg("/b_allocRead", [self.bufferId, './temp_' + str(self.bufferId)])
-       # print(self.sc.client.recv())
-
-    def get_id(self):
-        return self.bufferId
+        self.sr = sr
+        sp.io.wavfile.write('./temp_' + str(self.bufnum), sr, data)
+        self.sc.msg("/b_allocRead", [self.bufnum, './temp_' + str(self.bufnum)])
+        #print(self.sc.client.recv())
 
     def play(self, synth="pb"):
-        self.sc.msg("/s_new", [synth, -1, 1, 0, "bufnum", self.bufferId, "rate", 40])
+        self.sc.msg("/s_new", [synth, -1, 1, 0, "bufnum", self.bufnum, "rate", self.sr])
 
     def free(self):
-        self.sc.msg("/b_free", [self.bufferId])
-        os.remove('./temp_' + str(self.bufferId))
+        self.sc.msg("/b_free", [self.bufnum])
+        os.remove('./temp_' + str(self.bufnum))
+
+    def fill(self, start, count, value):
+        self.sc.msg("/b_fill", [self.bufnum, [start, count, value]])
+
+    def write(self, path, header="wav", sample="float"):
+        self.sc.msg("/b_write", [self.bufnum, path, header, sample, -1, 0, 0])
+
+    def custom_command(self, command):
+        self.sc.msg("/b_gen", [self.bufnum, command])
+
+    def info(self):
+        self.sc.msg("/b_query", [self.bufnum])
+        # ToDo: Wait for fix sync problem
+        return self.sc.client.recv()
 
     def __del__(self):
         self.free()
