7718a386e9ded3ed3ca6b106ac99be1c000a9fbc
Steffen Micha Voße <github@gympap.de>
2019-05-17 15:43:50 +0200

    Buffer: - add example notebook - some restructure in buffer class

--- sc3nb/buffer.py
+++ sc3nb/buffer.py
@@ -3,51 +3,50 @@ import os
 import scipy.io.wavfile
 from random import randint
 
+# ToDo: Add blocking to all loader
 
 class Buffer:
     """
-    Initialize a buffer. You can choose:
-    b = Buffer(sr*) -> empty buffer, which is not initialized in SC, create it afterwards with b.file(...), b.alloc(...) etc.
-    b = Buffer(data, datamode*, sr*) -> create a buffer out of a numpy array
-    b = Buffer(path, sr*) -> create a buffer out of a .wav file
-    b = Buffer(size, sr*) -> Create an empty buffer with given size
-    * Optinal parameter
+    Initialize a buffer. You have to fill the buffer afterwards:
+    b = Buffer().load_file(...)
+    b = Buffer().load_data(...)
+    b = Buffer().alloc(...)
+    b = Buffer().load_pya(...)
+    b = Buffer().load_existing(...)
+    b = Buffer().copy(Buffer)
 
     when you pass asyncron = True and create a buffer while initialization, the initialization will wait until is the buffer is created in SC
     """
-    def __init__(self, sc, sr=44100, data=None, size=None, datamode=None, path=None, bufmode=None, asyncron=False):
-        self._bufnum = randint(0, 100000)  #ToDo: Better num handling: use nextNodeID after PR
+    def __init__(self, sc):
+        self._bufnum = randint(0, 100000)  # ToDo: Better num handling: use nextNodeID after PR
         self.sc = sc
-        self.sr = sr
-        self._bufmode = bufmode
+        self.sr = None
+        self._bufmode = None
         self._loaded = False
         self._tempfile = None
         self._path = None
 
-        if bufmode == 'file':
-            Buffer.file(self, path)
-        if bufmode == 'alloc':
-            Buffer.alloc(self, size)
-        if bufmode == 'data':
-            Buffer.data(self, data, datamode)
-
-        if asyncron:
-            return self.sc.client.recv()
-
     # Section: Initialization
-    def file(self, path):
+    def load_file(self, path, sr=44100):
         self._bufmode = 'file'
+        self.sr = sr # ToDo: Read sr from file, for example: scipy.io.wavfile.read -> rate
         self._path = path
         self.sc.msg("/b_allocRead", [self._bufnum, path])
         self._loaded = True
 
-    def alloc(self, size):
+        return self
+
+    def alloc(self, size, sr=44100):
+        self.sr = sr
         self._bufmode = 'alloc'
         self.sc.msg("/b_alloc", [self._bufnum, size])
         self._loaded = True
 
-    def data(self, data, mode='file'):
+        return self
+
+    def load_data(self, data, mode='file', sr=44100):
         self._bufmode = 'data'
+        self.sr = sr
         if mode == 'file':
             if not os.path.exists('./temp/'):
                 os.makedirs('./temp/')
@@ -58,6 +57,18 @@ class Buffer:
             self.sc.msg("/b_alloc", [self._bufnum, data.shape[0]])
             self.sc.msg("/b_setn", [self._bufnum, data.tolist()])
         self._loaded = True
+        return self
+
+    def load_pya(self, pya, mode='file'):
+        Buffer.data(self, pya.asic, mode, sr=pya.sample_rate)
+        return self
+
+    def load_existing(self, bufnum, sr=44100):
+        self._bufmode = 'existing'
+        self.sr = sr
+        self._bufnum = bufnum
+        self._loaded = True
+        return self
 
     # Section: Modify buffer
     def fill(self, start, count, value):
@@ -65,16 +76,16 @@ class Buffer:
             raise Exception("Buffer object is not initialized yet!")
         self.sc.msg("/b_fill", [self._bufnum, [start, count, value]])
 
-    def custom_command(self, command):
+    def gen(self, command):
         if self._loaded is False:
             raise Exception("Buffer object is not initialized yet!")
         self.sc.msg("/b_gen", [self._bufnum, command])
 
     # Section: Output
-    def play(self, synth="pb", loop=False):
+    def play(self, synth="pb", rate=1, loop=False):
         if self._loaded is False:
             raise Exception("Buffer object is not initialized yet!")
-        self.sc.msg("/s_new", [synth, -1, 1, 0, "bufnum", self._bufnum, "rate", self.sr, "loop", 1 if loop else 0])
+        self.sc.msg("/s_new", [synth, -1, 1, 0, "bufnum", self._bufnum, "rate", rate, "loop", 1 if loop else 0])
 
     def write(self, path, header="wav", sample="float"):
         if self._loaded is False:
@@ -90,9 +101,9 @@ class Buffer:
         return self.sc.client.recv()
 
     def __repr__(self):
-        return "Buffer(sc, sr=" + str(self.sr) + ", bufmode=" + str(self._bufmode) + ") \r\n Loaded=" + str(self._loaded) + \
-               " \r\n Bufnum=" + str(self._bufnum) + \
-               " \r\n To see more information about the buffer data, use Buffer.info()"
+        return f"Buffer(sc, sr={str(self.sr)}, bufmode={str(self._bufmode)}) \r\n Loaded={str(self._loaded)}" + \
+                "\r\n Bufnum={str(self._bufnum)}" + \
+                "\r\n To see more information about the buffer data, use Buffer.info()"
 
     # Section: Delete/ free buffer
     def free(self):
