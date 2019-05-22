b18846da13ee88ff86afab8cc090ec218d165ff3
Thomas Hermann <thermann@techfak.uni-bielefeld.de>
2019-05-22 23:23:27 +0200

    - added class docstring - moved zero to modify section - added return self to some methods - merge pr into develop when query() problem is resolved.

--- sc3nb/buffer.py
+++ sc3nb/buffer.py
@@ -7,15 +7,75 @@ import time
 # ToDo: Add blocking to all loader
 
 class Buffer:
+
     """
-    Initialize a buffer. You have to fill the buffer afterwards:
+    A Buffer object represents a SuperCollider3 Buffer on scsynth
+    and provides access to low-level buffer commands of scsynth via
+    methods of the Buffer objects.
+
+    The constructor merely initializes a buffer:
+    * it selects a buffer number using SC's buffer allocator
+    * it initializes attribute variables
+
+    For more information on Buffer commands in sc3, refer to the Server Command
+    Reference in sc3 help.
+
+    Parameters
+    ----------
+    sc : object of type SC
+        The server instance to establish the Buffer to 
+
+    Attributes
+    ----------
+    sc : the SC object 
+        to communicate with scsynth
+    sr : int
+        the sampling rate of the buffer
+    bufnum : int
+        buffer number = bufnum id on scsynth
+    bufmode : {'file', 'osc'}
+        transport mode for data from python to scsynth. Defaults to 'file'
+    path : string
+        path to a audio file as used in load_file()
+    tempfile : string
+        filename (if created) of temporary file used for data transfer to scsynth
+    allocated : boolean
+        flagged True if Buffer has been allocated by
+        any of the initialization methods
+
+    See Also
+    --------
+    --
+
+    Notes
+    -----
+    The default way of allocating a Buffer would be 
+        b1 = scn.Buffer(sc).alloc(44100)
+    For convenience, a method named Buffer() (sic: capital 'B') 
+    has been added to class SC which forwards self to calling 
+    the Buffer constructor, allowing instead to write
+        b1 = sc.Buffer().alloc(44100)
+
+    Examples
+    --------
     b = Buffer().load_file(...)
     b = Buffer().load_data(...)
     b = Buffer().alloc(...)
     b = Buffer().load_pya(...)
     b = Buffer().load_existing(...)
     b = Buffer().copy(Buffer)
+
+    Raises
+    ------
+    Exception: [description]
+    
+    Returns
+    -------
+    self : object of type Buffer
+        the created Buffer object
+    
     """
+
     def __init__(self, sc):
         self._bufnum = sc.nextBufferID()
         self.sc = sc
@@ -25,7 +85,7 @@ class Buffer:
         self._tempfile = None
         self._path = None
 
-    # Section: Initialization
+    # Section: Buffer initialization methods
     def load_file(self, path):
         self._bufmode = 'file'
         file = sp.io.wavfile.read(path)
@@ -33,7 +93,6 @@ class Buffer:
         self._path = path
         self.sc.msg("/b_allocRead", [self._bufnum, path])
         self._allocated = True
-
         return self
 
     def alloc(self, size, sr=44100):
@@ -41,7 +100,6 @@ class Buffer:
         self._bufmode = 'alloc'
         self.sc.msg("/b_alloc", [self._bufnum, size])
         self._allocated = True
-
         return self
 
     def load_data(self, data, mode='file', sr=44100):
@@ -61,6 +119,7 @@ class Buffer:
 
     def load_collection(self, data, mode='file', sr=44100):
         self.load_data(data, mode, sr)
+        return self
 
     def load_pya(self, pya, mode='file'):
         Buffer.data(self, pya.asic, mode, sr=pya.sample_rate)
@@ -82,37 +141,46 @@ class Buffer:
         self.load_file(filepath)
         return self
 
-    # Section: Modify buffer
+    # Section: Buffer modification methods
     def fill(self, start, count, value):
         if self._allocated is False:
             raise Exception("Buffer object is not initialized yet!")
         self.sc.msg("/b_fill", [self._bufnum, [start, count, value]])
+        return self
 
     def gen(self, command):
         if self._allocated is False:
             raise Exception("Buffer object is not initialized yet!")
         self.sc.msg("/b_gen", [self._bufnum, command])
+        return self
 
-    # Section: Output
+    def zero(self):
+        if self._allocated is False:
+            raise Exception("Buffer object is not initialized yet!")
+        self.sc.msg("/b_zero", [self._bufnum])
+        return self
+
+    # Section: Buffer output methods
     def play(self, synth="pb-1ch", rate=1, loop=False, pan=0, amp=0.3):
         if self._allocated is False:
             raise Exception("Buffer object is not initialized yet!")
         id = self.sc.nextNodeID()
         self.sc.msg("/s_new", [synth, id, 1, 1,
-                               "bufnum", self._bufnum,
-                               "rate", rate,
-                               "loop", 1 if loop else 0,
-                               "pan", pan,
-                               "amp", amp
-                               ])
+            "bufnum", self._bufnum,
+            "rate", rate,
+            "loop", 1 if loop else 0,
+            "pan", pan,
+            "amp", amp
+            ])
         return id
 
     def write(self, path, header="wav", sample="float"):
         if self._allocated is False:
             raise Exception("Buffer object is not initialized yet!")
         self.sc.msg("/b_write", [self._bufnum, path, header, sample, -1, 0, 0])
+        return self
 
-    # Section: Buffer information
+    # Section: Buffer information methods
     def query(self):
         if self._allocated is False:
             raise Exception("Buffer object is not initialized yet!")
@@ -125,18 +193,13 @@ class Buffer:
                f"\r\n Bufnum={str(self._bufnum)}" + \
                 "\r\n To see more information about the buffer data, use Buffer.info()"
 
-    # Section: Delete/ free buffer
+    # Section: Methods to delete / free Buffers
     def free(self):
         if self._allocated is False:
             raise Exception("Buffer object is not initialized yet!")
         self.sc.msg("/b_free", [self._bufnum])
         if self._bufmode == 'data' and isinstance(self._tempfile, str):
             os.remove(self._tempfile)
-
-    def zero(self):
-        if self._allocated is False:
-            raise Exception("Buffer object is not initialized yet!")
-        self.sc.msg("/b_zero", [self._bufnum])
 
     def __del__(self):
         self.free()
