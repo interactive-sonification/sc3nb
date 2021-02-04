27e049837c080965c1d705633ff1cc5595e7a1ef
Dennis Reinsch <dreinsch@techfak.uni-bielefeld.de>
2021-02-04 14:43:14 +0100

    add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
 with open('requirements.txt') as read_file:
     REQUIRED = read_file.read().splitlines()
 
@@ -7,6 +10,8 @@ setup(
     name='sc3nb',
     version='0.0.1',
     description='SuperCollider3 (sc3) jupyter notebook class and magics',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     license='MIT',
     packages=find_packages(exclude=["tests"]),
     package_data={
