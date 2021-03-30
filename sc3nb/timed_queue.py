e9219159c732fe1df2a0936c30366263b8555dea
Dennis Reinsch <dreinsch@techfak.uni-bielefeld.de>
2021-03-30 13:07:29 +0200

    Change docstrings to numpydoc and add typing

--- sc3nb/timed_queue.py
+++ sc3nb/timed_queue.py
@@ -2,27 +2,38 @@
 
 import threading
 import time
+from typing import Any, Callable, Iterable, NoReturn, Union
 
 import numpy as np
+from pythonosc.osc_message import OscMessage
 
 import sc3nb
+from sc3nb.osc.osc_communication import Bundler, OSCCommunication
 
 
 class Event:
     """Stores a timestamp, function and arguments for that function.
     Long running functions can be wrapped inside an own thread
 
-    Arguments:
-        timetag {int} -- Time event should be executed
-        function {[type]} -- Function to be executed
-        args {[type]} -- Arguments for function
-
-    Keyword Arguments:
-        spawn {bool} -- if True, create new sub-thread
-                        for function (default: {False})
+    Parameters
+    ----------
+    timetag : int
+        Time event should be executed
+    function : Callable[..., None]
+        Function to be executed
+    args : Iterable[Any]
+        Arguments for function
+    spawn : bool, optional
+        if True, create new thread for function, by default False
     """
 
-    def __init__(self, timetag, function, args, spawn=False):
+    def __init__(
+        self,
+        timetag: int,
+        function: Callable[..., None],
+        args: Iterable[Any],
+        spawn: bool = False,
+    ) -> None:
         if spawn:
             thread = threading.Thread(target=function, args=args)
             function = thread.start
@@ -31,7 +42,7 @@ class Event:
         self.function = function
         self.args = args
 
-    def execute(self):
+    def execute(self) -> None:
         """Executes function"""
         self.function(*self.args)
 
@@ -49,20 +60,28 @@ class Event:
 
 
 class TimedQueue:
-    """Accumulates events as timestamps and functions. Executes given
-    functions according to the timestamps
+    """Accumulates events as timestamps and functions.
 
-    Keyword Arguments:
-        relative_time {bool} -- if True, use relative
-                                time (default: {False})
-        thread_sleep_time {float} -- Sleep time for
-                                     worker thread
-                                     (default: {0.001})
+    Executes given functions according to the timestamps
+
+    Parameters
+    ----------
+    relative_time : bool, optional
+        If True, use relative time, by default False
+    thread_sleep_time : float, optional
+        Sleep time for worker thread, by default 0.001
+    drop_time_threshold : float, optional
+        Threshold for execution time of events.
+        If this is exceeded the event will be dropped, by default 0.5
     """
 
-    def __init__(self, relative_time=False, thread_sleep_time=0.001, drop_time_thr=0.5):
-
-        self.drop_time_thr = drop_time_thr
+    def __init__(
+        self,
+        relative_time: bool = False,
+        thread_sleep_time: float = 0.001,
+        drop_time_threshold: float = 0.5,
+    ) -> None:
+        self.drop_time_thr = drop_time_threshold
         if relative_time:
             self.start = time.time()
         else:
@@ -80,42 +99,46 @@ class TimedQueue:
 
         self.thread.start()
 
-    def close(self):
-        """Closes event processing without waiting for
-        pending events to complete
-        """
+    def close(self) -> None:
+        """Closes event processing without waiting for pending events"""
         self.close_event.set()
         self.thread.join()
 
-    def join(self):
-        """Closes event processing after waiting for
-        pending events to complete
-        """
+    def join(self) -> None:
+        """Closes event processing after waiting for pending events"""
         self.complete()
         self.close_event.set()
         self.thread.join()
 
-    def complete(self):
+    def complete(self) -> None:
         """Blocks until all pending events have completed"""
         while self.event_list:
             time.sleep(0.01)
 
-    def put(self, timetag, function, args=(), spawn=False):
+    def put(
+        self,
+        timetag: int,
+        function: Callable[..., None],
+        args: Iterable[Any] = (),
+        spawn: bool = False,
+    ) -> None:
         """Adds event to queue
 
-        Arguments:
-            timetag {int} -- Time when event should be
-                             executed
-            function {callable} -- Function to be executed
+        Parameters
+        ----------
+        timetag : int
+            Time when event should be executed
+        function : Callable[..., None]
+            Function to be executed
+        args : Iterable[Any], optional
+            Arguments to be passed to function, by default ()
+        spawn : bool, optional
+            if True, create new sub-thread for function, by default False
 
-        Keyword Arguments:
-            args {tuple} -- Arguments to be passed to function
-                            (default: {()})
-            spawn {bool} -- if True, create new sub-thread
-                            for function (default: {False})
-
-        Raises:
-            TypeError -- raised if function is not callable
+        Raises
+        ------
+        TypeError
+            raised if function is not callable
         """
 
         if not hasattr(function, "__call__"):
@@ -134,35 +157,41 @@ class TimedQueue:
                 self.onset_idx, idx, [timetag, evlen - 1], axis=0
             )
 
-    def get(self):
+    def get(self) -> Event:
         """Get latest event from queue and remove event
 
-        Returns:
-            Event -- Latest event
+        Returns
+        -------
+        Event
+            Latest event
         """
         event = self.peek()
         self.pop()
         return event
 
-    def peek(self):
+    def peek(self) -> Event:
         """Look up latest event from queue
 
-        Returns:
-            Event -- latest event
+        Returns
+        -------
+        Event
+            Latest event
         """
         with self.lock:
             return self.event_list[int(self.onset_idx[0][1])]
 
-    def empty(self):
+    def empty(self) -> bool:
         """Checks if queue is empty
 
-        Returns:
-            bool -- True if queue if empty
+        Returns
+        -------
+        bool
+            True if queue if empty
         """
         with self.lock:
             return bool(self.event_list)
 
-    def pop(self):
+    def pop(self) -> None:
         """Removes latest event from queue"""
         with self.lock:
             event_idx = int(self.onset_idx[0][1])
@@ -171,7 +200,7 @@ class TimedQueue:
             self.onset_idx[:, 1][self.onset_idx[:, 1] > event_idx] -= 1
             del self.event_list[event_idx]
 
-    def __worker(self, sleep_time, close_event):
+    def __worker(self, sleep_time: float, close_event: threading.Event) -> NoReturn:
         """Worker function to process events"""
         while True:
             if close_event.is_set():
@@ -189,18 +218,66 @@ class TimedQueue:
     def __repr__(self):
         return self.event_list.__repr__()
 
-    def elapse(self, time_delta):
+    def elapse(self, time_delta: float) -> None:
+        """Add time delta to the current queue time.
+
+        Parameters
+        ----------
+        time_delta : float
+            Additional time
+        """
         self.start += time_delta
 
 
 class TimedQueueSC(TimedQueue):
-    def __init__(self, server=None, relative_time=False, thread_sleep_time=0.001):
+    """Timed queue with OSC communication.
+
+    Parameters
+    ----------
+    server : OSCCommunication, optional
+        OSC server to handle the bundlers and messsages, by default None
+    relative_time : bool, optional
+        If True, use relative time, by default False
+    thread_sleep_time : float, optional
+        Sleep time for worker thread, by default 0.001
+    """
+
+    def __init__(
+        self,
+        server: OSCCommunication = None,
+        relative_time: bool = False,
+        thread_sleep_time: float = 0.001,
+    ):
         super().__init__(relative_time, thread_sleep_time)
         self.server = server or sc3nb.SC.get_default().server
 
-    def put_bundle(self, onset, timetag, address, args):
-        callback = self.server.bundler(timetag, address, args).send
-        self.put(onset, callback)
+    def put_bundler(self, onset: int, bundler: Bundler) -> None:
+        """Add a Bundler to queue
 
-    def put_msg(self, onset, address, args):
-        self.put(onset, self.server.msg, args=(address, args))
+        Parameters
+        ----------
+        onset : int
+            Sending timetag of the Bundler
+        bundler : Bundler
+            Bundler that will be send
+        """
+        self.put(onset, bundler.send)
+
+    def put_msg(
+        self, onset: int, msg: Union[OscMessage, str], args: Iterable[Any]
+    ) -> None:
+        """Add a message to queue
+
+        Parameters
+        ----------
+        onset : int
+            Sending timetag of the message
+        msg : Union[OscMessage, str]
+            OscMessage or OSC address
+        args : Iterable[Any]
+            If msg is str, this will be the arguments of the created OscMessage
+        """
+        if isinstance(msg, str):
+            self.put(onset, self.server.msg, args=(msg, args))
+        else:
+            self.put(onset, self.server.send, args=(msg,))
