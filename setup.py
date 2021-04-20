176bfb98bb389d62712f953ac3b77e019f8500b8
Dennis Reinsch <dreinsch@techfak.uni-bielefeld.de>
2021-04-20 14:32:03 +0200

    Change to setup.cfg from setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,30 @@
-from setuptools import find_packages, setup
+# Minimal setup.py to allow editable installs via pip intstall --editable .
+# See https://setuptools.readthedocs.io/en/latest/userguide/quickstart.html#development-mode
+import setuptools
 
-with open("README.md", "r", encoding="utf-8") as file:
-    long_description = file.read()
+setuptools.setup()
 
-with open("requirements.txt") as read_file:
-    REQUIRED = read_file.read().splitlines()
 
-setup(
-    name="sc3nb",
-    version="0.0.1",
-    description="SuperCollider3 (sc3) jupyter notebook class and magics",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    license="MIT",
-    packages=find_packages(exclude=["tests"]),
-    install_requires=REQUIRED,
-    author="Thomas Hermann",
-    author_email="thermann@techfak.uni-bielefeld.de",
-    keywords=["sonification, sound synthesis"],
-    url="https://github.com/thomas-hermann/sc3nb",
-)
+### old
+# from setuptools import find_packages, setup
+
+# with open("README.md", "r", encoding="utf-8") as file:
+#     long_description = file.read()
+
+# with open("requirements.txt") as read_file:
+#     REQUIRED = read_file.read().splitlines()
+
+# setup(
+#     name="sc3nb",
+#     version="0.0.1",
+#     description="SuperCollider3 (sc3) jupyter notebook class and magics",
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     license="MIT",
+#     packages=find_packages(exclude=["tests"]),
+#     install_requires=REQUIRED,
+#     author="Thomas Hermann",
+#     author_email="thermann@techfak.uni-bielefeld.de",
+#     keywords=["sonification, sound synthesis"],
+#     url="https://github.com/thomas-hermann/sc3nb",
+# )
