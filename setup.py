788a128b98410f69248d9b17e9d4daa25af410f0
Dennis Reinsch <dreinsch@techfak.uni-bielefeld.de>
2021-05-24 14:58:46 +0200

    Fix setuptools config

--- setup.py
+++ setup.py
@@ -3,28 +3,3 @@
 import setuptools
 
 setuptools.setup()
-
-
-### old
-# from setuptools import find_packages, setup
-
-# with open("README.md", "r", encoding="utf-8") as file:
-#     long_description = file.read()
-
-# with open("requirements.txt") as read_file:
-#     REQUIRED = read_file.read().splitlines()
-
-# setup(
-#     name="sc3nb",
-#     version="0.0.1",
-#     description="SuperCollider3 (sc3) jupyter notebook class and magics",
-#     long_description=long_description,
-#     long_description_content_type="text/markdown",
-#     license="MIT",
-#     packages=find_packages(exclude=["tests"]),
-#     install_requires=REQUIRED,
-#     author="Thomas Hermann",
-#     author_email="thermann@techfak.uni-bielefeld.de",
-#     keywords=["sonification, sound synthesis"],
-#     url="https://github.com/thomas-hermann/sc3nb",
-# )
