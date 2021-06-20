02097622c313d1eaae3e57ace78c08da8c21ed96
Dennis Reinsch <dreinsch@techfak.uni-bielefeld.de>
2021-06-20 18:42:21 +0200

    Disable running notebooks at readthedocs for now

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,19 +3,20 @@
 # For a full list of options see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
 
 import json
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
 import os
 import sys
 from pathlib import Path
 
 from pkg_resources import get_distribution
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
 
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
@@ -32,6 +33,34 @@ version = ".".join(release.split(".")[:2])
 html_context = dict(versions=str(version))
 
 # -- General configuration ---------------------------------------------------
+
+on_rtd = os.environ.get("READTHEDOCS") == "True"
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+html_theme_options = {"collapse_navigation": False}
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ["_static"]
+html_css_files = [
+    "css/custom.css",
+]  # custom css to break long signatures. ref https://github.com/sphinx-doc/sphinx/issues/1514
+
+# -- Options for extensions --------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -71,34 +100,12 @@ nbsphinx_execute_arguments = [
     "--InlineBackend.figure_formats={'png', 'svg', 'pdf'}",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+# TODO currently readthedocs uses ubuntu18.04, this is to old
+# https://docs.readthedocs.io/en/stable/config-file/v2.html#build-apt-packages
+if on_rtd:
+    nbsphinx_execute = "never"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
-html_theme_options = {"collapse_navigation": False}
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-html_css_files = [
-    "css/custom.css",
-]  # custom css to break long signatures. ref https://github.com/sphinx-doc/sphinx/issues/1514
-
-
-###############
-# Custom code #
-###############
+# -- Custom code -------------------------------------------------------------
 
 # Preparing notebooks
 
