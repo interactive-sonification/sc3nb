38c731919bce6f2f5580c11f8dc9c6105655891c
Dennis Reinsch <dreinsch@techfak.uni-bielefeld.de>
2021-06-21 20:35:21 +0200

    Update docs

--- refactoring/_static/documentation_options.js
+++ refactoring/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.1.dev108+gd1612a0',
+    VERSION: '0.1.1.dev118+g466e4ca',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
